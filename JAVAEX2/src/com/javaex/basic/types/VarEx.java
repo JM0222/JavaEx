09f8540acd5577a6d4f7dfb0dad4dc7069bf4875
JM <wkdwoals96@naver.com>
2021-02-25 16:09:32 +0900

    변수 선언과 데이터 타입

---
+++ JAVAEX2/src/com/javaex/basic/types/VarEx.java
@@ -0,0 +1,22 @@
+package com.javaex.basic.types;
+
+// 변수 연습
+
+public class VarEx { 
+	public static void main(String[] args) {
+		// 변수 식별자 규정
+		// 문자 , 숫자 , $ , _의 조합으로 생성
+		// 숫자로 시작하면 안된다
+		// 예약어는 사용할수 없다
+		
+		// 관례:
+		// 	    소문자로 작성, 단어의 조합일 경우, 단어의 첫글자는 대문자
+		
+		// 초기화
+		int myAge; // int 데이터 할당
+		myAge = 25;
+		System.out.println("나이는" + myAge);
+		
+	}
+
+}
