44773ba9c0d80fb0598efb1a6717db61112972a3
JM <wkdwoals96@naver.com>
2021-02-25 14:47:02 +0900

    Initial Commit

---
+++ JAVAEX2/src/com/javaex/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.javaex;
+//주석
+/* 여러줄 주석 */
+public class HelloWorld {
+//	객체가 실행하고자 한다면 main 메서드가 필요
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
