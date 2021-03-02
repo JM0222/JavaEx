cadfb09356288deca98518c6a8f810ea4947f6e7
JM <wkdwoals96@gmail.com>
2021-03-02 19:18:58 +0900

    practice

--- JAVAEX2/src/com/javaex/practice/Problem03.java
+++ JAVAEX2/src/com/javaex/practice/Problem03.java
@@ -7,10 +7,11 @@ public class Problem03 {
 
 	}
 	public static void ex03() {
-		for (int i = 2; i <= 9; i++) {
-			for (int j = 1; j <= 9; j++) {
-				System.out.println(i+"*"+j+"="+(i*j));
+		for (int i = 1; i <= 9; i++) {
+			for (int j = 2; j <= 9; j++) {
+				System.out.print(j+"*"+i+"="+(i*j)+"\t");
 			}
+			System.out.println();
 		}
 	}
 
