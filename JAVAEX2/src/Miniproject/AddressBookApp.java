7a9606ad855a255477f388734fea1f47a0f4b811
JM0222 <wkdwoals96@gmail.com>
2021-03-12 17:06:23 +0900

    Miniproject ex

---
+++ JAVAEX2/src/Miniproject/AddressBookApp.java
@@ -0,0 +1,11 @@
+package Miniproject;
+
+public class AddressBookApp {
+
+	public static void main(String[] args) {
+		AddressBookController adr = new AddressBookController();
+		adr.addressBook();
+
+	}
+
+}
